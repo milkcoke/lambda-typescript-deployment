dd3453573400400f6a88f6c8f627035f4300394f
milkcoke <mbh023@gmail.com>
2023-06-01 10:45:28 +0900

    feat: Add getObject S3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 import { APIGatewayEvent, Context } from 'aws-lambda';
+import {S3Client} from '@aws-sdk/client-s3'
+import {GetObjectCommand} from "@aws-sdk/client-s3";
+
+const s3Client = new S3Client({
+    region: 'ap-northeast-2'
+})
 export async function handler (event: APIGatewayEvent, context: Context) {
-    console.dir(event)
+    const responseBody = await s3Client.send(new GetObjectCommand({
+        Bucket: process.env.S3Bucket,
+        Key: process.env.S3Key
+    })).then(res=>Buffer.from(res.Body).toString('utf-8'))
+
+    console.dir(responseBody)
 }
