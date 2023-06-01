63f532fcb2250353661ddda26d7e506da2798f6c
milkcoke <mbh023@gmail.com>
2023-06-01 14:38:09 +0900

    feat: Add s3 event trigger logging

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import { APIGatewayEvent, Context } from 'aws-lambda';
+import { S3Event, Context } from 'aws-lambda';
 import {S3Client} from '@aws-sdk/client-s3'
 import {GetObjectCommand} from "@aws-sdk/client-s3";
 
 const s3Client = new S3Client({
     region: 'ap-northeast-2'
 })
-export async function handler (event: APIGatewayEvent, context: Context) {
-    const responseBody = await s3Client.send(new GetObjectCommand({
-        Bucket: process.env.S3Bucket,
-        Key: process.env.S3Key
-    })).then(res=>Buffer.from(res.Body).toString('utf-8'))
+export async function handler (s3Event: S3Event, context: Context) {
+    console.dir(s3Event.Records)
+    const s3Record = s3Event.Records[0]
+    console.dir(s3Record)
 
-    console.dir(responseBody)
+    // const res = await s3Client.send(new GetObjectCommand({
+    //     Bucket: process.env.S3Bucket,
+    //     Key: process.env.S3Key
+    // })).then(res=>res.Body as ReadableStream)
 }
